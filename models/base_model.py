25872f04db5ac02b7bf73b27296005040de7c5d5
Byron523 <byrongumbo23@outlook.com>
2023-02-08 22:22:38 +0200

    updated the base model

--- models/base_model.py
+++ models/base_model.py
@@ -10,9 +10,9 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """ An init method that instantiates files """
         if not kwargs:
-            self.id = str(uuid.uuid4)
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
         else:
             form = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
@@ -23,9 +23,8 @@ class BaseModel:
 
     def __str__(self):
         """ This method prints dict, id and class """
-        cls_name = "[" + self.__class__.__name__ + "]"
-        nw_dct = {k: v for (k, v) in self.__dict__.items() if (not v) if False}
-        return (cls_name + "(" + self.id + ")" + str(nw_dct))
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """ Updates the updated_at instance """
